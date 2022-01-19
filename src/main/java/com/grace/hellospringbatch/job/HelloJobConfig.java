46aba4ea0c65e47ab95f0607583792ba102e0215
berryberry189 <berryberry189@naver.com>
2022-01-19 23:03:16 +0900

    간단한 배치 만들어보기

---
+++ src/main/java/com/grace/hellospringbatch/job/HelloJobConfig.java
@@ -0,0 +1,49 @@
+package com.grace.hellospringbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean("helloJob")
+    public Job helloJob(Step helloStep){
+        return jobBuilderFactory.get("helloJob")
+                .incrementer(new RunIdIncrementer())
+                .start(helloStep)
+                .build();
+
+    }
+
+    @JobScope // 어플리케이션이 살아있는동안 동안 계속 떠있을 필요가 없기때문에, Job이 살아있는동안만 떠있도록 scope를 설정해준다
+    @Bean("helloStep")
+    public Step helloStep(Tasklet tasklet){
+        return stepBuilderFactory.get("helloStep")
+                .tasklet(tasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet tasklet(){
+        return (contribution, chunkContext) -> {
+            System.out.println("hello Spring Batch!");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
